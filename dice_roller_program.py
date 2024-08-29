1f5e3cabd4b8b44043ddbc7679ece8e72f237fa4
bilal-sajid <ahmadbilalsajid01@gmail.com>
2024-08-28 22:53:04 -0400

    Create dice_roller_program.py
    A dice rolling program with simple visuals

---
+++ dice_roller_program.py
@@ -0,0 +1,69 @@
+import random
+
+# ● ┌ ─ ┐ │ └ ┘
+
+# "┌─────────┐"
+# "│         │"
+# "│         │"
+# "│         │"
+# "└─────────┘"
+
+dice_art = {
+    1: ("┌─────────┐",
+        "│         │",
+        "│    ●    │",
+        "│         │",
+        "└─────────┘"),
+    2: ("┌─────────┐",
+        "│  ●      │",
+        "│         │",
+        "│      ●  │",
+        "└─────────┘"),
+    3: ("┌─────────┐",
+        "│ ●       │",
+        "│    ●    │",
+        "│       ● │",
+        "└─────────┘"),
+    4: ("┌─────────┐",
+        "│ ●     ● │",
+        "│         │",
+        "│ ●     ● │",
+        "└─────────┘"),
+    5: ("┌─────────┐",
+        "│ ●     ● │",
+        "│    ●    │",
+        "│ ●     ● │",
+        "└─────────┘"),
+    6: ("┌─────────┐",
+        "│ ●     ● │",
+        "│ ●     ● │",
+        "│ ●     ● │",
+        "└─────────┘")
+}
+
+
+dice = []
+total = 0
+num_of_dice = int(input("How many times do you want to roll the dice? : "))
+
+print()
+print("Your rolls: \n")
+
+for die in range(num_of_dice):
+    dice.append(random.randint(1,6))
+
+# for die in range(num_of_dice):
+#     for line in dice_art.get(dice[die]):
+#         print(line)
+
+for line in range(5):
+    for die in dice:
+        print(dice_art.get(die)[line], end=" ")
+    print()
+
+for roll in range(num_of_dice):
+    print(f"In turn {roll+1}, you rolled a {dice[roll]}")
+
+for die in dice:
+    total += die
+print(f"Your Total: {total}")
