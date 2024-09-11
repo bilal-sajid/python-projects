f0c865b8ef0376c15a64b0ab9e5f85b13d6626a8
bilal-sajid <ahmadbilalsajid01@gmail.com>
2024-09-11 18:19:50 -0400

    NATO Alphabet Project
    Simple Project to showcase list comprehensions and dictionary comprehensions

---
+++ NATO Alphabet Project/main.py
@@ -0,0 +1,13 @@
+import pandas
+
+file_path = "NATO Alphabet Project/nato_phonetic_alphabet.csv"
+data = pandas.read_csv(file_path)
+
+phonetic_dict = {row.letter:row.code for (index,row) in data.iterrows()}
+
+
+word = input("Enter a word: ").upper()
+
+output_list = [phonetic_dict[letter] for letter in word if letter != " "]
+print(output_list)
+
