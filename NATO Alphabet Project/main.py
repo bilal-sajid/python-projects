cbf8962b9602a392d9363a0d6fb6fca4230636eb
bilal-sajid <ahmadbilalsajid01@gmail.com>
2024-09-13 23:43:10 -0400

    Update to NATO Project
    Integrating Exception Handling for the project

--- NATO Alphabet Project/main.py
+++ NATO Alphabet Project/main.py
@@ -6,10 +6,19 @@ data = pandas.read_csv(file_path)
 phonetic_dict = {row.letter:row.code for (index,row) in data.iterrows()}
 
 
-word = input("Enter a word: ").upper()
 
-output_list = [phonetic_dict[letter] for letter in word if letter != " "]
-print(output_list)
+def generate_phonetic():
+    word = input("Enter a word: ").upper()
 
+    try:
+        output_list = [phonetic_dict[letter] for letter in word if letter != " "]
 
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        generate_phonetic()
+
+    else:
+        print(output_list)
+
+generate_phonetic()
 
